0d1a50b9c12767816bdc1f51d4b21fef9a1aa1c9
Simon Ninon <simon.ninon@gmail.com>
2016-07-07 23:27:17 +0200

    reimplement the TCP client using raw socket API

--- includes/cpp_redis/network/tcp_client.hpp
+++ includes/cpp_redis/network/tcp_client.hpp
@@ -18,58 +18,61 @@ namespace network {
 //! async tcp client based on boost asio
 class tcp_client {
 public:
-    //! ctor & dtor
-    tcp_client(void);
-    ~tcp_client(void);
+  //! ctor & dtor
+  tcp_client(void);
+  ~tcp_client(void);
 
-    //! assignment operator & copy ctor
-    tcp_client(const tcp_client&) = delete;
-    tcp_client& operator=(const tcp_client&) = delete;
+  //! assignment operator & copy ctor
+  tcp_client(const tcp_client&) = delete;
+  tcp_client& operator=(const tcp_client&) = delete;
 
-    //! returns whether the client is connected or not
-    bool is_connected(void);
+  //! returns whether the client is connected or not
+  bool is_connected(void);
 
-    //! handle connection & disconnection
-    void connect(const std::string& host, unsigned int port);
-    void disconnect(void);
+  //! handle connection & disconnection
+  void connect(const std::string& host, unsigned int port);
+  void disconnect(void);
 
-    //! send data
-    void send(const std::string& buffer);
-    void send(const std::vector<char>& buffer);
+  //! send data
+  void send(const std::string& buffer);
+  void send(const std::vector<char>& buffer);
 
-    //! set receive callback
-    //! called each time some data has been received
-    //! Returns true if everything is ok, false otherwise (trigger disconnection)
-    typedef std::function<bool(tcp_client&, const std::vector<char>& buffer)> receive_handler;
-    void set_receive_handler(const receive_handler& handler);
+  //! set receive callback
+  //! called each time some data has been received
+  //! Returns true if everything is ok, false otherwise (trigger disconnection)
+  typedef std::function<bool(tcp_client&, const std::vector<char>& buffer)> receive_handler;
+  void set_receive_handler(const receive_handler& handler);
 
-    //! set disconnection callback
-    //! called each time a disconnection has been detected
-    typedef std::function<void(tcp_client&)> disconnection_handler;
-    void set_disconnection_handler(const disconnection_handler& handler);
-
-private:
-    //! make boost asio async read and write operations
-    void async_read(void);
-    void async_write(void);
+  //! set disconnection callback
+  //! called each time a disconnection has been detected
+  typedef std::function<void(tcp_client&)> disconnection_handler;
+  void set_disconnection_handler(const disconnection_handler& handler);
 
 private:
-    //! is connected
-    std::atomic_bool m_is_connected;
+  //! make boost asio async read and write operations
+  void async_read(void);
+  void async_write(void);
 
-    //! buffers
-    static const unsigned int READ_SIZE = 2048;
-    std::vector<char> m_read_buffer;
-    std::vector<char> m_write_buffer;
+private:
+  //! socket fd
+  int m_fd;
 
-    //! handlers
-    receive_handler m_receive_handler;
-    disconnection_handler m_disconnection_handler;
+  //! is connected
+  std::atomic_bool m_is_connected;
 
-    //! thread safety
-    std::mutex m_write_buffer_mutex;
-    std::mutex m_receive_handler_mutex;
-    std::mutex m_disconnection_handler_mutex;
+  //! buffers
+  static const unsigned int READ_SIZE = 2048;
+  std::vector<char> m_read_buffer;
+  std::vector<char> m_write_buffer;
+
+  //! handlers
+  receive_handler m_receive_handler;
+  disconnection_handler m_disconnection_handler;
+
+  //! thread safety
+  std::mutex m_write_buffer_mutex;
+  std::mutex m_receive_handler_mutex;
+  std::mutex m_disconnection_handler_mutex;
 };
 
 } //! network
--- sources/network/tcp_client.cpp
+++ sources/network/tcp_client.cpp
@@ -1,4 +1,6 @@
 #include <condition_variable>
+#include <netdb.h>
+#include <cstring>
 
 #include "cpp_redis/network/tcp_client.hpp"
 
@@ -6,18 +8,59 @@ namespace cpp_redis {
 
 namespace network {
 
-tcp_client::tcp_client(void) {}
+tcp_client::tcp_client(void)
+: m_fd(-1)
+, m_is_connected(false)
+, m_read_buffer(READ_SIZE)
+, m_receive_handler(nullptr)
+, m_disconnection_handler(nullptr)
+{}
 
-tcp_client::~tcp_client(void) {}
+tcp_client::~tcp_client(void) {
+  if (m_is_connected)
+    io_service::get_instance().untrack(m_fd);
+}
 
 void
-tcp_client::connect(const std::string& host, unsigned int port) {}
+tcp_client::connect(const std::string& host, unsigned int port) {
+  if (m_is_connected)
+    return ;
+
+  //! create the socket
+  m_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (m_fd < 0)
+      throw redis_error("Can't open a socket");
+
+  //! get the server's DNS entry
+  struct hostent *server = gethostbyname(host.c_str());
+  if (not server)
+    throw redis_error("No such host: " + host);
+
+  //! build the server's Internet address
+  struct sockaddr_in server_addr;
+  std::memset(&server_addr, 0, sizeof(server_addr));
+  std::memcpy(server->h_addr, &server_addr.sin_addr.s_addr, server->h_length);
+  server_addr.sin_port = htons(port);
+  server_addr.sin_family = AF_INET;
+
+  //! create a connection with the server
+  if (::connect(m_fd, reinterpret_cast<const struct sockaddr *>(&server_addr), sizeof(server_addr)) < 0)
+    throw redis_error("Fail to connect to " + host + ":" + std::to_string(port));
+
+  m_is_connected = true;
+  io_service::get_instance().track(m_fd);
+  async_read();
+}
 
 void
-tcp_client::disconnect(void) {}
+tcp_client::disconnect(void) {
+  if (not m_is_connected)
+    return ;
 
-void
-tcp_client::async_read(void) {}
+  m_is_connected = false;
+  io_service::get_instance().untrack(m_fd);
+  close(m_fd);
+}
 
 void
 tcp_client::send(const std::string& buffer) {
@@ -25,10 +68,65 @@ tcp_client::send(const std::string& buffer) {
 }
 
 void
-tcp_client::send(const std::vector<char>& buffer) {}
+tcp_client::send(const std::vector<char>& buffer) {
+  if (not m_is_connected)
+      throw redis_error("Not connected");
+
+  if (not buffer.size())
+      return ;
+
+  std::lock_guard<std::mutex> lock(m_write_buffer_mutex);
+
+  bool bytes_in_buffer = m_write_buffer.size() > 0;
+
+  //! concat buffer
+  m_write_buffer.insert(m_write_buffer.end(), buffer.begin(), buffer.end());
+
+  //! if there were already bytes in buffer, simply return
+  //! async_write callback will process the new buffer
+  if (bytes_in_buffer)
+      return;
+
+  async_write();
+}
 
 void
-tcp_client::async_write(void) {}
+tcp_client::async_read(void) {
+  io_service::get_instance().async_read(m_fd, m_read_buffer, READ_SIZE,
+    [=](bool success, std::size_t length) {
+       if (not success) {
+           disconnect();
+           return ;
+       }
+
+       std::lock_guard<std::mutex> lock(m_receive_handler_mutex);
+       if (m_receive_handler)
+           if (not m_receive_handler(*this, { m_read_buffer.begin(), m_read_buffer.begin() + length })) {
+               disconnect();
+               return ;
+           }
+
+       //! keep waiting for incoming bytes
+       async_read();
+    });
+}
+
+void
+tcp_client::async_write(void) {
+  io_service::get_instance().async_write(m_fd, m_write_buffer, m_write_buffer.size(),
+    [this](bool success, std::size_t length) {
+        if (not success) {
+            disconnect();
+            return ;
+        }
+
+        std::lock_guard<std::mutex> lock(m_write_buffer_mutex);
+        m_write_buffer.erase(m_write_buffer.begin(), m_write_buffer.begin() + length);
+
+        if (m_write_buffer.size())
+            async_write();
+    });
+}
 
 void
 tcp_client::set_receive_handler(const receive_handler& handler) {
