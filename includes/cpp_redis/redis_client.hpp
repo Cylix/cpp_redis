d2c73b0737f009e9ae3056d81926aab6fce892f0
Lucas Terra <lucasterra@users.noreply.github.com>
2016-11-18 19:14:22 -0200

    allow custom callback before calling reply_callback_t (#26)

--- includes/cpp_redis/redis_client.hpp
+++ includes/cpp_redis/redis_client.hpp
@@ -32,6 +32,7 @@ public:
 
   //! send cmd
   typedef std::function<void(reply&)> reply_callback_t;
+  redis_client& before_callback(const std::function<void(reply&, const reply_callback_t& callback)>& callback);
   redis_client& send(const std::vector<std::string>& redis_cmd, const reply_callback_t& callback = nullptr);
 
   //! commit pipelined transaction
@@ -277,6 +278,9 @@ private:
 
   //! user defined disconnection handler
   disconnection_handler_t m_disconnection_handler;
+
+  //! user defined before callback handler
+  std::function<void(reply&, reply_callback_t& callback)> m_before_callback_handler;
 
   //! thread safety
   std::mutex m_callbacks_mutex;
