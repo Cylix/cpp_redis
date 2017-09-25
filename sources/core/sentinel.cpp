2e14c0b9b87155a872b8e7f8c0fe5935bfd1c227
Simon Ninon <simon.ninon@gmail.com>
2017-09-25 16:38:00 +0200

    fix #112

--- sources/core/sentinel.cpp
+++ sources/core/sentinel.cpp
@@ -29,13 +29,15 @@
 namespace cpp_redis {
 
 #ifndef __CPP_REDIS_USE_CUSTOM_TCP_CLIENT
-sentinel::sentinel(void) {
+sentinel::sentinel(void)
+: m_callbacks_running(0) {
   __CPP_REDIS_LOG(debug, "cpp_redis::sentinel created");
 }
 #endif /* __CPP_REDIS_USE_CUSTOM_TCP_CLIENT */
 
 sentinel::sentinel(const std::shared_ptr<network::tcp_client_iface>& tcp_client)
-: m_client(tcp_client) {
+: m_client(tcp_client)
+, m_callbacks_running(0) {
   __CPP_REDIS_LOG(debug, "cpp_redis::sentinel created");
 }
 
